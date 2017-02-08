6cbb6f68d38ba4f70b9f160ab47e5e6109dd0f53
utente <utente@NBCHIWYL0003>
2017-02-08 09:09:07 +0100

    Aggiunto peso

--- js/controllers/pressioneController.js
+++ js/controllers/pressioneController.js
@@ -1,14 +1,14 @@
 app.controller('PressioneController',["jsonService",function (jsonService){
         var vm = this;
         
+       
+        
         
         vm.init = function(){
             jsonService.getPressione(function(response){
                 console.log(response.data);
                 vm.list = response.data;
-            });
-            
-            
+            });  
         };
         
         vm.init();
